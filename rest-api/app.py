83c7071fb37716158e2e02998a7ad2c0b33a20b9
dawid <dwloszek@gmail.com>
2019-05-20 21:02:46 +0200

    updates

---
+++ rest-api/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'dawid'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+items = []
+
+class Item(Resource):
+    @jwt_required
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    @jwt_required
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': 'Item {) is already created'.format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    @jwt_required
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name),items)
+        return {'message': 'Item deleted'}
+
+    @jwt_required
+    def put(self,name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name']==name, items),None)
+        if not item:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
---
+++ rest-api/security.py
@@ -0,0 +1,24 @@
+from werkzeug.security import safe_str_cmp
+from user import User
+
+users = [
+    User(1,'bob','asdf')
+]
+
+username_mapping = { user.username: user for user in users}
+userid_mapping = { user.id: user for user in users}
+
+userid_mapping = { 1: {
+    'id': 1,
+    'username': 'bob',
+    'password': 'asdf'
+}}
+
+def authenticate(username, password):
+    user = username_mapping.get(username,None)
+    if user and safe_str_cmp(user.password,password):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id,None)
